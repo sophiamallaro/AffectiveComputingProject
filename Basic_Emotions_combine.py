7394e52f6ca837c4170a66f96d361db9a7a32ca9
Sophia Mallaro <sophia-mallaro@uiowa.edu>
2018-11-19 19:05:05 -0800

    Update Basic_Emotions_combine.py

--- Basic_Emotions_combine.py
+++ Basic_Emotions_combine.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 from PyLyrics import *
 import nltk
 #import indicoio
@@ -15,107 +9,64 @@ import numpy as np
 import torch
 import lib
 from ConvNet import ConvNet
+from getUserSongs import *
+import lyricsgenius as genius
 
+def get_basic_ming(): 
+    dictionary = get_dictionary()
+    
 
-# In[2]:
+def get_song_emotions():
+    api = genius.Genius('9mXsJ6OfC-KdM2QF1xl_0hRVZ7KiqrQYtUwobdB4kcpVsClOHUGf_d1a8qQjfIoa')
+    emotion_dict = np.load('emotion_dict.npy')
+    emotion_dict = emotion_dict.item()
+    names = get_dictionary()
 
+    PATH = "model/model.pth"
+    num_classes = 6
+    model = ConvNet(num_classes)
+    model.load_state_dict(torch.load(PATH))
+    model.eval()
 
-emotion_dict = np.load('emotion_dict.npy')
-emotion_dict = emotion_dict.item() 
-names = np.load('songs.npy')
-names = names.item()
+    print("load models")
 
+    songs_emo = {}
+    for k,v in names.items():
+        print(k,v)
+        title = v[0]
+        artist = v[1]
+        text_emotions = [0 for i in range(6)]
+        output = [0 for i in range(6)]
+        pred = [0 for i in range(6)]
+        try:
+            lyrics = PyLyrics.getLyrics(artist, title)
+            if lyrics == "":
+                song = api.search_song(artist, title)
+                lyrics = song.lyrics
+            emovector = lib.emotion_analyzer(lyrics, emotion_dict)
+            high = max(emovector)
 
-# In[3]:
-
-
-PATH = "model/model.pth"
-num_classes = 6
-model = ConvNet(num_classes)
-model.load_state_dict(torch.load(PATH))
-model.eval()
-
-
-# In[4]:
-
-
-
-songs_emo = {}
-for k,v in names.items():
-    title = v[0]
-    artist = v[1]
-    text_emotions = [0 for i in range(6)]
-    output = [0 for i in range(6)]
-    pred = [0 for i in range(6)]
-    try:
-        lyrics = PyLyrics.getLyrics(artist, title)
-        emovector = lib.emotion_analyzer(lyrics, emotion_dict)
-        high = max(emovector)
-        
-        text_emotions = [0 if i!=high else 1 for i in emovector]
-        text_emotions = np.array(text_emotions)
-    except:
-        pass
-    try:
+            text_emotions = [0 if i!=high else 1 for i in emovector]
+            text_emotions = np.array(text_emotions)
+        except:
+            print("passing")
+        #try:
         mp3file = "data/" + k + ".mp3"
-        
-        spec = lib.audio_read(mp3file)# extract spectrogram 
+
+        spec = lib.audio_read(mp3file)# extract spectrogram
+        spec = torch.from_numpy(spec)
         pred = model(spec)
         high = max(pred)
         pred = [0 if i!=high else 1 for i in pred]
         pred = np.array(pred)
-    except:
-        pass
-        
-    output = np.add(pred, text_emotions)
-    np.clip(output,0,1,out=output)
-    songs_emo[k] = output
-    print('finishing '+k)
-    
+        output = np.add(pred, text_emotions)
+        np.clip(output,0,1,out=output)
+        songs_emo[k] = output
+        print('finishing '+k)
 
+    return songs_emo
 
-# In[5]:
-
-
-
-
-        
-
-
-# In[8]:
-
-
-ans = lib.get_rec([0,0,0,0,1,1])
-
-
-# In[9]:
-
-
-ans
-
-
-# In[11]:
-
-
-
-
-
-# In[13]:
-
-
-from getUserSongs import *
-sp = generate_token()
-make_playlist(sp, "sad", ans)
-
-
-# In[15]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
+def get_recomendations(user_emotion):
+    all_songs = get_song_emotions()
+    ans = lib.get_rec(user_emotion,all_songs)
+    return ans
